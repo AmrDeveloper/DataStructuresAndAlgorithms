9a8a02c58d9a74fef8ea7102818af5919a8e26d7
Amr Hesham Gaber <amr96@programmer.net>
2017-09-05 16:16:09 +0300

    Update BinaryTree.java

--- Java/Graph/BinaryTree.java
+++ Java/Graph/BinaryTree.java
@@ -1,69 +1,87 @@
-//The Right Node Is always Bigger Than The Main 
-//The Left Node Is always Smaller Than The Main
-//In Binary Tree Remember Parent Only Have 2 child
+
 package Graph;
 
+//Binary Tree is tree of node with some rules
+//root node must bigger than right son 
+//and smaller than left son
+    
 
 public class BinaryTree {
-    //This Is The Main Node -First Father ^_^- 
-     SingleNode Root; 
-    //Constactor
-    BinaryTree(SingleNode Root){
-        this.Root = Root;
-    }
-    public void AddNode(SingleNode NewNode ,SingleNode ExplorNode){
-        if(ExplorNode == null)
-        {  
-            //No Father ^_^
-            return;
-        }
-        if(NewNode.value < ExplorNode.value)
-        {
-            if(ExplorNode.Left == null)
-            {
-                ExplorNode.Left = NewNode;
-            }
-            else
-            { //Change Father By Child.left As Father And Searsh Again
-            ExplorNode = ExplorNode.Left;
-            AddNode(NewNode , ExplorNode);
-            }
-        }
-        else if (NewNode.value > ExplorNode.value)
-        {   
-            //if no Right Son add new node
-            if(ExplorNode.Right == null)
-            {
-               ExplorNode.Right = NewNode; 
-            }
-            else
-            {
-              //Change Father By Child.Right As Father And Searsh Again
-              ExplorNode = ExplorNode.Right;
-              AddNode(NewNode , ExplorNode);  
-            }    
+    
+    //Node Class
+    public static class node{
+        //node value
+        private int value;
+        //left son node
+        private node left;
+        //right son node
+        private node right;
+        //Node Constructor
+        node(int value , node left , node right){
+            this.value = value;
+            this.left = left;
+            this.right = right;
         }
     }
     
-    public void Search(int value ,SingleNode ExplorNode){
-        //Cant Find Value
-         if(ExplorNode == null){
-            System.out.println("Cant Find " + value);
-            return;
+    //root is the father 
+    //and in binary three always have 2 son
+    node root;
+    BinaryTree(node root){
+        this.root = root;
+    }
+    
+    //Add Node To Binary Tree
+    public void add(node newNode , node explor){
+        //no father
+        if(root == null){
+            return ;
         }
-         //Find Value Done
-        if(value == ExplorNode.value){
-            System.out.println("Done Sir Your Val Is " + value);
-            return;
-        }   
-        //Value Small Than Root So GO Left
-        if(value < ExplorNode.value){
-           Search(value , ExplorNode.Left); 
+        //newNode Smaller than explor value
+        if(root.value > newNode.value){
+            //no left son
+            if(root.left == null)
+            {
+                //add to left son
+                root.left = newNode;
+            }
+            else
+
+            {
+                add(newNode , explor.left);
+            }
         }
-        //Value Small Than Root So GO Right
-        if(value > ExplorNode.value){
-            Search(value , ExplorNode.Right);
+        //newNode Bigger than explor value
+        if(root.value < newNode.value){
+            if(root.left == null)
+            {
+                //add to right son
+                root.right = newNode;
+            }
+            else
+            {
+                add(newNode , explor.right);
+            }
         }
     }
-  
+    //Search node on Binary Tree
+    public boolean search(int val , node explor){
+        //if end of node and not find it return false
+        if(explor == null){
+            return false;
+        }
+        //if value == val return true
+        if(explor.value == val){
+            return true;
+        }
+        //if value bigger than val go to left side
+        if(explor.value > val){
+            search(val , explor.left);
+        }
+        //if value smaller than val go to right side
+        if(explor.value < val){
+            search(val , explor.right);
+        }
+        return false;
+    }
 }
