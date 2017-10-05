e1eb42d2a8e0a423a66a2351043a8ba1c63b457c
Amr Hesham Gaber <amr96@programmer.net>
2017-10-05 17:47:16 +0200

    Update BinaryTreeDepth.java

--- Java/Graph/BinaryTreeDepth.java
+++ Java/Graph/BinaryTreeDepth.java
@@ -1,14 +1,18 @@
 
-package TODO;
+package Algorithms.Graph;
 
 
 public class BinaryTreeDepth {
     
     //node
     public static class node{
+        //node value
         int value;
+        //node -> right
         node right;
+        //node -> right
         node left;
+        //Constructor
         node(int value){
             this.value = value;
         }
@@ -16,13 +20,16 @@ public class BinaryTreeDepth {
     
     //root node
     private node root;
+    //node depth counter
     private int counter;
     
+    //Constructor
     BinaryTreeDepth(node root){
         this.root = root;
         this.counter = 0;
     }
     
+    //add new node and return node depth as integer
     public int addNode(node newNode , node explor){
         //for first time count root so counter start from 1
         if(explor == root){
@@ -68,6 +75,25 @@ public class BinaryTreeDepth {
         return counter; 
     }
     
-    
-    
+    //search for integer in tree
+    public boolean search(int val , node explor){
+        //no explor mean no root
+        if(explor ==  null){
+            return false;
+        }
+        //return true if find it
+        if(val == explor.value){
+            return true;
+        }
+        //if val bigger than explor search right
+        if(val > explor.value){
+            search(val,explor.right);
+        }
+        //if val smaller than explor search left
+        if(val < explor.value){
+            search(val,explor.left);
+        }
+        //else return false;
+        return false;
+    }
 }
