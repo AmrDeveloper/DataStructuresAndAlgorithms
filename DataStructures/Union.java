a85b2425f06edf82b8f846cf12f12d8d35773b46
AmrDeveloper <Amr96@programmer.net>
2019-05-06 21:52:20 +0200

    Create Union Data structure like in C

---
+++ DataStructures/Union.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+
+//Union Data Structure like union in C programming language
+//when you add value for key, all other values will be null
+
+public class Union<K, V> {
+
+    private K lastKeyChanged;
+    private HashMap<K, V> valuesMap;
+
+    public Union() {
+        this.valuesMap = new HashMap<>();
+    }
+
+    public void add(K key, V value) {
+        valuesMap.put(key, value);
+        if (lastKeyChanged != null) {
+            valuesMap.put(lastKeyChanged, null);
+        }
+        lastKeyChanged = key;
+    }
+
+    public V get(K key){
+        return valuesMap.get(key);
+    }
+}
